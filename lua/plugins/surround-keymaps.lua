1a042664b6ab3a8405956979752a28b12cf42bb8
bingoct <dollarsbingo@gmail.com>
2024-04-24 13:59:25 +0800

    update: surround keymap change

---
+++ lua/plugins/surround-keymaps.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "ys",
+        delete = "ds",
+        find = "fs",
+        find_left = "fsF",
+        highlight = "gsh",
+        replace = "cs",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+}
