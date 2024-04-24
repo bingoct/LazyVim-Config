bab11f52fe445e6dee449de049a3cbd6a0d7bbf3
bingoct <dollarsbingo@gmail.com>
2024-04-24 14:00:09 +0800

    update: disable theme plugin

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    enabled = false,
+    lazy = true,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    enabled = false,
+    lazy = true,
+  },
+}
