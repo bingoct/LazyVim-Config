fbfc91cca6c61f112ba90cdda08d45d61996957b
bingoct <dollarsbingo@gmail.com>
2024-04-24 13:39:49 +0800

    feat: colorschema set to onedark

---
+++ lua/plugins/colorschema_onedarkpro.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    lazy = false,
+    opts = {
+      styles = {
+        types = "NONE",
+        methods = "NONE",
+        numbers = "NONE",
+        strings = "NONE",
+        comments = "bold",
+        keywords = "italic",
+        constants = "NONE",
+        functions = "italic",
+        operators = "NONE",
+        variables = "NONE",
+        parameters = "NONE",
+        conditionals = "italic",
+        virtual_text = "NONE",
+      },
+    },
+  },
+}
