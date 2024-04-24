b61614829b9c7bac7b8cf90e352fc8965991b026
bingoct <dollarsbingo@gmail.com>
2024-04-24 13:28:46 +0800

    feat: options update
    add wsl clipboard
    set wrap and linebreak

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+--  clipboard
+local is_wsl = vim.fn.has("wsl") == 1
+if is_wsl then
+  vim.g.clipboard = {
+    name = "wsl",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
+
+--  wrap and linebreak
+vim.wo.wrap = true
+vim.wo.linebreak = true
